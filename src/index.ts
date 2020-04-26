e4ffcdd8a745fe0dffe464df6bf26382c515c14b
Eugene Sadovoi <evgeni@eniks.com>
2020-04-25 18:39:24 -0700

    Disabling failing tests

--- src/index.ts
+++ src/index.ts
@@ -2,91 +2,83 @@ import { Application } from 'probot'
 import { Octokit } from '@octokit/rest'
 import { LabelEnumerator, LabelHookPayload, INodeInfo } from './labels'
 
-
 export = (app: Application) => {
-
-  app.on('label',  async (context) => {
-    
+  app.on('label', async (context) => {
     // Dismiss if called recursively
-    if (context.isBot) return;
+    if (context.isBot) return
 
     // Parameters
-    let payload = context.payload as LabelHookPayload;
-    let login  = payload.organization.login;
-    let source = payload.repository.name;
-    let target = payload.changes?.name 
-               ? payload.changes.name.from
-               : payload.label.name;
+    const payload = context.payload as LabelHookPayload
+    const login = payload.organization.login
+    const source = payload.repository.name
+    const target = payload.changes?.name
+      ? payload.changes.name.from
+      : payload.label.name
 
-    // Select and create action           
-    var action = "deleted" == payload.action 
+    // Select and create action
+    var action = payload.action == 'deleted'
 
-    ? // Delete labels action
-    (info: INodeInfo) => {
-      if (null != info.label) {
+      ? // Delete labels action
+      (info: INodeInfo) => {
+        if (info.label != null) {
+          const options: Octokit.IssuesDeleteLabelParams = {
+            owner: login,
+            repo: info.name,
+            name: payload.label.name
+          }
 
-        let options: Octokit.IssuesDeleteLabelParams = {
-          owner: login,
-          repo: info.name,
-          name: payload.label.name,
-        };        
-
-        context.github.issues.deleteLabel(options)
-                             .catch((e: Octokit.HookError) => {
-                               context.log.error(e)
-                              });
+          context.github.issues.deleteLabel(options)
+            .catch((e: Octokit.HookError) => {
+              context.log.error(e)
+            })
+        }
       }
-    } 
 
-    : // Modify labels action
-    async (info: INodeInfo) => {
-      if (null == info.label) {
-
+      : // Modify labels action
+      async (info: INodeInfo) => {
+        if (info.label == null) {
         // Create label
 
-        let options: Octokit.IssuesCreateLabelParams = {
-          owner: login,
-          repo: info.name,
-          name: payload.label.name,
-          color: payload.label.color,
-          description: (payload.label as any)['description']
-        };
+          const options: Octokit.IssuesCreateLabelParams = {
+            owner: login,
+            repo: info.name,
+            name: payload.label.name,
+            color: payload.label.color,
+            description: (payload.label as any).description
+          }
 
-        context.github.issues.createLabel(options)
-                             .catch((e: Octokit.HookError) => {
+          context.github.issues.createLabel(options)
+            .catch((e: Octokit.HookError) => {
+              if (e.status != 422) {
+                context.log.error(e)
+                return
+              }
 
-                               if (422 != e.status) {
-                                 context.log.error(e);
-                                 return;
-                               }
-
-                               // Resend as update request
-                               let update = options as any;
-                               update.current_name = options.name;
-                               context.github.issues.updateLabel(update);
-                              });
-
-      } else {
-        
+              // Resend as update request
+              const update = options as any
+              update.current_name = options.name
+              context.github.issues.updateLabel(update)
+            })
+        } else {
         // Update label
 
-        let options: Octokit.IssuesUpdateLabelParams = {
-          owner: login,
-          repo: info.name,
-          current_name: target,
-          name: payload.label.name,
-          color: payload.label.color,
-          description: (payload.label as any)['description']
-        };
+          const options: Octokit.IssuesUpdateLabelParams = {
+            owner: login,
+            repo: info.name,
+            current_name: target,
+            name: payload.label.name,
+            color: payload.label.color,
+            description: (payload.label as any).description
+          }
 
-        context.github.issues.updateLabel(options)
-                             .catch((e: Octokit.HookError) => {
-                               context.log.error(e)
-                              });
+          context.github.issues.updateLabel(options)
+            .catch((e: Octokit.HookError) => {
+              context.log.error(e)
+            })
+        }
       }
-    }
- 
+
     // Process nodes
-    for await (let node of LabelEnumerator(context.github, login, source, target)) action(node);
+    for await (const node of LabelEnumerator(context.github, login, source, target)) action(node)
   })
 }
--- src/labels.ts
+++ src/labels.ts
@@ -1,9 +1,9 @@
 import { GitHubAPI } from 'probot'
-import { 
-  WebhookPayloadLabelLabel, 
-  PayloadRepository, 
-  WebhookPayloadLabelSender, 
-  WebhookPayloadTeamAddOrganization 
+import {
+  WebhookPayloadLabelLabel,
+  PayloadRepository,
+  WebhookPayloadLabelSender,
+  WebhookPayloadTeamAddOrganization
 } from '@octokit/webhooks'
 
 export interface ChangedFrom {
@@ -11,7 +11,7 @@ export interface ChangedFrom {
 }
 
 export interface PayloadLabelChanges {
-  name: ChangedFrom  | undefined;
+  name: ChangedFrom | undefined;
   color: ChangedFrom | undefined;
 }
 
@@ -50,7 +50,6 @@ export interface IRepoQueryPayload {
   };
 }
 
-
 const query = `query labels($login: String!, $name: String!, $cursor: String) {
     organization(login: $login) {
       repositories(first: 100, after: $cursor) {
@@ -71,40 +70,31 @@ const query = `query labels($login: String!, $name: String!, $cursor: String) {
     }
   }`
 
-  
-export async function* LabelEnumerator(github: GitHubAPI, login: string, repo: string, name: string) {
+export async function * LabelEnumerator (github: GitHubAPI, login: string, repo: string, name: string) {
+  const tracker = {
+    login: login,
+    name: name,
+    cursor: null as any
+  }
 
-    let tracker = {
-        "login":  login,
-        "name":   name,
-        "cursor": null as any,
-    };
-    
-    try 
-    {
-        let data: IRepoQueryPayload;
-        let promise = github.graphql(query, tracker);
+  try {
+    let data: IRepoQueryPayload
+    let promise = github.graphql(query, tracker)
 
-        do 
-        {
-            // Get paged list of repositories
-            data = await promise as IRepoQueryPayload;
-            if (data.organization.repositories.pageInfo.hasNextPage) {
-                tracker.cursor = data.organization.repositories.pageInfo.endCursor;
-                promise = github.graphql(query, tracker);
-            }
-            
-            for (let node of data.organization.repositories.nodes) {
-              if (node.name == repo) continue;
-              yield node;
-            }
+    do {
+      // Get paged list of repositories
+      data = await promise as IRepoQueryPayload
+      if (data.organization.repositories.pageInfo.hasNextPage) {
+        tracker.cursor = data.organization.repositories.pageInfo.endCursor
+        promise = github.graphql(query, tracker)
+      }
 
-        } while(data.organization.repositories.pageInfo.hasNextPage);
-        
-
-    }
-    catch(e)
-    {
-        console.log(e);
-    }
+      for (const node of data.organization.repositories.nodes) {
+        if (node.name == repo) continue
+        yield node
+      }
+    } while (data.organization.repositories.pageInfo.hasNextPage)
+  } catch (e) {
+    console.log(e)
+  }
 }
