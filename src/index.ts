370404187971686ad10c51eb17d5b294e5570763
Eugene Sadovoi <evgeni@eniks.com>
2020-04-26 12:07:46 -0700

    refactor: Adding comments and changing names

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Application } from 'probot'
-import { DeleteLabelAction, ModifiedLabelAction } from './labels'
+import { DeleteLabelAction as DeletedLabelAction, ModifiedLabelAction } from './labels'
 import { LabelEnumerator, LabelHookPayload } from './repositories'
 
 export = (app: Application) => {
   app.on('label', async (context) => {
-    // Dismiss if called recursively
-    if (context.isBot) return
+    
+    if (context.isBot) return; // Dismiss if called recursively
 
     // Parameters
     const payload = context.payload as LabelHookPayload
@@ -14,12 +14,12 @@ export = (app: Application) => {
                  ? payload.changes.name.from
                  : payload.label.name
 
-    // Select and create action
+    // Select and create action to perform
     var action = payload.action == 'deleted'
-               ? DeleteLabelAction(context, login, payload.label.name)
+               ? DeletedLabelAction(context, login, payload.label.name)
                : ModifiedLabelAction(context, login, target, payload.label);
 
-    // Process nodes
+    // Process all repositories
     for await (const node of LabelEnumerator(context, target)) action(node)
   })
 }
