0464238eb9e586441bdd503ad7c36c97dadd6efb
Eugene Sadovoi <evgeni@eniks.com>
2020-04-27 15:35:42 -0700

    Adding tracers (#5)
    * Adding documentation
    
    * docs: Added home page
    
    * docs:
    
    * docs: complete page
    
    * docs: rephrasing
    
    * chore: adding trace info

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,21 @@ var botLoginName: string;
 export = (app: Application) => {
  
   app.on('label', async (context) => {
+    context.log.trace(`Received event ${context.event} from ${context.payload.sender.login}`);
 
     // Get login info of the bot
     if (!botLoginName) {
+      context.log.trace(`Querying for bot login name`);
+      
       const botUser = await context.github.query(`{ viewer { login } }`);
       botLoginName = botUser?.viewer.login;
+
+      context.log.trace(`Bot logged as ${botLoginName}`);
     }
 
     // Dismiss if called recursively 
     if (context.isBot && context.payload.sender.login == botLoginName) {
+      context.log.trace(`Recursive call, dismissing`);
       return; 
     }
 
@@ -25,10 +31,12 @@ export = (app: Application) => {
     const target = payload.changes?.name
                  ? payload.changes.name.from
                  : payload.label.name
+                 
+    context.log.trace(`Organization: ${login}, Label: ${target}`);
 
     // Select and create action to perform
     var action = payload.action == 'deleted'
-               ? DeletedLabelAction(context, login, payload.label.name)
+               ? DeletedLabelAction( context, login, payload.label.name)
                : ModifiedLabelAction(context, login, target, payload.label);
 
     // Process all repositories
