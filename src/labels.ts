b2997280fc81aa252aa5bcc10ae85029461b1c7e
Eugene Sadovoi <evgeni@eniks.com>
2020-04-28 16:52:14 -0700

    style: Adjusting code style

--- src/labels.ts
+++ src/labels.ts
@@ -27,48 +27,48 @@ export function ModifiedLabelAction(context: Context<WebhookPayloadLabel>, login
     if (info.label == null) {
       // Create label
 
-        const options: Octokit.IssuesCreateLabelParams = {
-          owner: login,
-          repo: info.name,
-          name: label.name,
-          color: label.color,
-          description: (label as any).description
-        }
+      const options: Octokit.IssuesCreateLabelParams = {
+        owner: login,
+        repo: info.name,
+        name: label.name,
+        color: label.color,
+        description: (label as any).description
+      }
 
-        context.github.issues
-                      .createLabel(options)
-                      .catch((e: Octokit.HookError) => {
-                        if (e.status != 422) {
-                          context.log.error(e)
-                          return
-                        }
+      context.github.issues
+                    .createLabel(options)
+                    .catch((e: Octokit.HookError) => {
+                      if (e.status != 422) {
+                        context.log.error(e)
+                        return
+                      }
 
-                        // Resend as update request
-                        const update = options as any
-                        update.current_name = options.name
-                        context.github.issues
-                                      .updateLabel(update)                        
-                                      .catch((e: Octokit.HookError) => {
-                                        context.log.error(e)
-                                      })
-                      })
+                      // Resend as update request
+                      const update = options as any
+                      update.current_name = options.name
+                      context.github.issues
+                                    .updateLabel(update)                        
+                                    .catch((e: Octokit.HookError) => {
+                                      context.log.error(e)
+                                    })
+                    })
       } else {
       // Update label
 
-        const options: Octokit.IssuesUpdateLabelParams = {
-          owner: login,
-          repo: info.name,
-          current_name: name,
-          name: label.name,
-          color: label.color,
-          description: (label as any).description
-        }
+      const options: Octokit.IssuesUpdateLabelParams = {
+        owner: login,
+        repo: info.name,
+        current_name: name,
+        name: label.name,
+        color: label.color,
+        description: (label as any).description
+      }
 
-        context.github.issues
-                      .updateLabel(options)
-                      .catch((e: Octokit.HookError) => {
-                        context.log.error(e)
-                      })
+      context.github.issues
+                    .updateLabel(options)
+                    .catch((e: Octokit.HookError) => {
+                      context.log.error(e)
+                    })
       }
     }
 }
